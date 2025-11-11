480492d2adcd13dc374b97889a9772807fe021bf
victoriapatarroyo <victoria.patarroyo@gmail.com>
2025-11-11 16:22:17 -0500

    Primera versión archivo JS

---
+++ script.js
@@ -0,0 +1,12 @@
+// Genera un color aleatorio en formato hexadecimal
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+let titulo = document.getElementById("titulo");
+titulo.style.color = getRandomColor();
